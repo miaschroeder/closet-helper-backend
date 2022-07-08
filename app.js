f5ade1ded6ae4e5035086c4b0e84accb6f70c1d9
Mia Schroeder <miaschroeder527@gmail.com>
2022-07-07 17:20:20 -0700

    Connected app to database

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+
+const port = process.env.PORT || 3001;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server listening on ${port}...`);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+start();
