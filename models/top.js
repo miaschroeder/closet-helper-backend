256893712694d55bce0f0cb2ebd8d7310737d8d4
Mia Schroeder <miaschroeder527@gmail.com>
2022-07-07 17:42:43 -0700

    Created schema for top documents

---
+++ models/top.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+
+const TopSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Must provide name of top'],
+        trim: true,
+        maxlength: [20, 'Name cannot be more than 20 characters']
+    },
+    style: {
+        type: String,
+        trim: true,
+        enum: ['casual', 'active', 'business']
+    },
+    weather: {
+        type: String,
+        trim: true,
+        enum: ['hot', 'warm', 'chilly', 'cold']
+    },
+    favorite: {
+        type: Boolean,
+        default: false,
+    }
+});
+
+module.exports = mongoose.model('Top', TopSchema);
